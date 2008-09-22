b06337491d2a99ac08f7dbcf1e8beb6449a0ccf2
ahmetaa <ahmetaa@ca822fc4-7eb6-11dd-8d1c-71c7e1663bfb>
2008-09-22 01:21:08 +0000

    firefix ve hunspell kaynak dosyalarini ekle.

---
+++ kaynaklar/firefox/install.js
@@ -0,0 +1,11 @@
+﻿var err = initInstall("Basic Turkish Dictionary", "tr@dictionaries.addons.mozilla.org", "0.1");
+if (err != SUCCESS)
+    cancelInstall();
+
+var fProgram = getFolder("Program");
+err = addDirectory("", "tr@dictionaries.addons.mozilla.org",
+		   "dictionaries", fProgram, "dictionaries", true);
+if (err != SUCCESS)
+    cancelInstall();
+
+performInstall();
